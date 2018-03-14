bde0b16455814b3a6038b6137903782d086c8200
anthonykoch <anthonykoch@users.noreply.github.com>
2018-03-14 03:52:11 -0700

    Fix lint

--- test/exec.test.js
+++ test/exec.test.js
@@ -1,30 +1,29 @@
 import test from 'ava';
 
-import path from 'path';
-
-import { run } from '../lib/transform';
+// import { run } from '../lib/transform';
 
 test.skip('exec.run() - executes js code and runs a callback for each expression', async t => {
+  t.end();
 
-  const functionId = 'LivelyJS';
-  const env = 'browser';
+  // const functionId = 'LivelyJS';
+  // const env = 'browser';
 
-  const code = `
-  `;
+  // const code = `
+  // `;
 
-  const result = await run(code, {
-    sourcemap,
-    functionId,
-    env,
-    __dirname: 'memeslol',
-    __filename: 'memeslol',
-    module: module,
-    insertions,
-    notifiers: {
-      expression: [
-        (id, value) => {}
-      ],
-    },
-  });
+  // const result = await run(code, {
+  //   sourcemap,
+  //   functionId,
+  //   env,
+  //   __dirname: 'memeslol',
+  //   __filename: 'memeslol',
+  //   module: module,
+  //   insertions,
+  //   notifiers: {
+  //     expression: [
+  //       (id, value) => {}
+  //     ],
+  //   },
+  // });
 
 });
